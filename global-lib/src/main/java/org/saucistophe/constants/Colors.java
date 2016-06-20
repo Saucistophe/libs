0dbdb97259e025f3d4409b636780ab450d156f2a
stophe <carpentier.ch@gmail.com>
2016-06-21 00:59:46 +0200

    [ALL] Added Kelly differentiable colors.

---
+++ global-lib/src/main/java/org/saucistophe/constants/Colors.java
@@ -0,0 +1,56 @@
+package org.saucistophe.constants;
+
+import java.awt.Color;
+
+/**
+ Useful Colors constants.
+ */
+public class Colors
+{
+	/**
+	 An array of highly differentiable colors.
+	 */
+	public static final Color[] KELLY_COLORS =
+	{
+		Color.decode("0xFFB300"), // Vivid Yellow
+		Color.decode("0x803E75"), // Strong Purple
+		Color.decode("0xFF6800"), // Vivid Orange
+		Color.decode("0xA6BDD7"), // Very Light Blue
+		Color.decode("0xC10020"), // Vivid Red
+		Color.decode("0xCEA262"), // Grayish Yellow
+		Color.decode("0x817066"), // Medium Gray
+
+		Color.decode("0x007D34"), // Vivid Green
+		Color.decode("0xF6768E"), // Strong Purplish Pink
+		Color.decode("0x00538A"), // Strong Blue
+		Color.decode("0xFF7A5C"), // Strong Yellowish Pink
+		Color.decode("0x53377A"), // Strong Violet
+		Color.decode("0xFF8E00"), // Vivid Orange Yellow
+		Color.decode("0xB32851"), // Strong Purplish Red
+		Color.decode("0xF4C800"), // Vivid Greenish Yellow
+		Color.decode("0x7F180D"), // Strong Reddish Brown
+		Color.decode("0x93AA00"), // Vivid Yellowish Green
+		Color.decode("0x593315"), // Deep Yellowish Brown
+		Color.decode("0xF13A13"), // Vivid Reddish Orange
+		Color.decode("0x232C16"),    // Dark Olive Green
+	};
+
+	/**
+	 The next color to get.
+	 */
+	public int index = 0;
+
+	/**
+	@return A contrasted color, and prepare for next one.
+	*/
+	public Color getNextColor()
+	{
+		Color result = KELLY_COLORS[index];
+		index++;
+		if (index >= KELLY_COLORS.length)
+		{
+			index = 0;
+		}
+		return result;
+	}
+}
