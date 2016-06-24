73bb2b91272b0fdc2bc6ac3a6a6aa07dc5636ff4
stophe <carpentier.ch@gmail.com>
2016-06-24 23:40:24 +0200

    |ALL] Switched alignment to something more sensible.

--- global-lib/src/main/java/org/saucistophe/constants/Alignment.java
+++ global-lib/src/main/java/org/saucistophe/constants/Alignment.java
@@ -3,15 +3,7 @@ package org.saucistophe.constants;
 /**
  A general-purpose alignment constants class.
  */
-public class Alignment
+public enum Alignment
 {
-	public enum Horizontal
-	{
-		LEFT, CENTER, RIGHT
-	}
-
-	public enum Vertical
-	{
-		BOTTOM, CENTER, TOP
-	}
+	LEADING,CENTER,TRAILING
 }
