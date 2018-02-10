79264c7c49dbf0027faa908db9fbc838baaa382e
stophe <carpentier.ch@gmail.com>
2018-02-10 18:31:25 +0100

    [ALL] Made logs a bit more legible, at least y my standards.

--- global-lib/src/main/java/org/saucistophe/logs/ColorConsoleHandler.java
+++ global-lib/src/main/java/org/saucistophe/logs/ColorConsoleHandler.java
@@ -32,22 +32,49 @@ public class ColorConsoleHandler extends ConsoleHandler
 				String prefix;
 				Level level = record.getLevel();
 				if (level == Level.SEVERE)
+				{
 					prefix = COLOR_SEVERE;
+				}
 				else if (level == Level.WARNING)
+				{
 					prefix = COLOR_WARNING;
+				}
 				else if (level == Level.INFO)
+				{
 					prefix = COLOR_INFO;
+				}
 				else if (level == Level.CONFIG)
+				{
 					prefix = COLOR_CONFIG;
+				}
 				else if (level == Level.FINE)
+				{
 					prefix = COLOR_FINE;
+				}
 				else if (level == Level.FINER)
+				{
 					prefix = COLOR_FINER;
+				}
 				else if (level == Level.FINEST)
+				{
 					prefix = COLOR_FINEST;
+				}
 				else
+				{
 					prefix = COLOR_SEVERE;
+				}
 
+				// Use short package names.
+				String[] pathElements = record.getSourceClassName().split("\\.");
+				for (int i = 0; i < pathElements.length - 1; i++)
+					pathElements[i] = pathElements[i].substring(0, 1);
+				
+
+				// I don't like spaces between class and metthod. I prefer ::.
+				record.setSourceMethodName(String.join("::", String.join(".",pathElements), record.getSourceMethodName()));
+				record.setSourceClassName(null);
+				
+				
 				String rawMessage = super.format(record);
 				return prefix + rawMessage + COLOR_RESET;
 			}
